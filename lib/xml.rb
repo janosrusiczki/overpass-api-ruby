301f556b324bfb85a0b734fa2f76d762fe07108b
Janos Rusiczki <janos.rusiczki@gmail.com>
2019-02-01 20:56:53 +0200

    Update lib/xml.rb to pass spec

--- lib/xml.rb
+++ lib/xml.rb
@@ -9,11 +9,11 @@ module OverpassAPI
     end
 
     def build_query(query)
-      bbox = @bbox ? "bbox='#{@bbox}'" : ''
-      timeout = @timeout ? "timeout='#{@timeout}'" : ''
-      element_limit = @element_limit ? "element-limit='#{@element_limit}'" : ''
+      bbox = @bbox ? " bbox='#{@bbox}'" : ''
+      timeout = @timeout ? " timeout='#{@timeout}'" : ''
+      element_limit = @element_limit ? " element-limit='#{@element_limit}'" : ''
 
-      "<osm-script #{bbox} #{timeout} #{element_limit} output='json'>"\
+      "<osm-script#{bbox}#{timeout}#{element_limit} output='json'>" \
       "#{query}<print/></osm-script>"
     end
   end
